7732afc702039e2db611ee5ef67eaf987ac749b1
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-06-06 16:10:53 +0530

    commit again

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,18 @@
 import productsList from './ProductsJSON';
-// import  { useState } from 'react';
 
-// export const [ initialState, setInitialState ] = useState([...productsList]);
 export const initialState = [
     ...productsList
+    // filterBrands: [...productsList]
 ]
 
-const reducer = (state = initialState, action) => {
+const reducer = (state, action) => {
     switch(action.type){
         case 'SORT_HIGH_TO_LOW':
             return state = action.sortHighToLow;
         case 'SORT_LOW_TO_HIGH':
             return state = action.sortLowToHigh;
+        case 'FILTER_WITH_BRAND':
+            return state = action.filterBrand;
         default:
             return state;
     }
